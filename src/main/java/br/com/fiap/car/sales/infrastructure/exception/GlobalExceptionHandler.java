de6086b8fd25842432da7e2c923670b6d31fe525
wienerdev <matheuswiener9@gmail.com>
2024-07-14 11:33:33 -0300

    Adicionando log no GlobalExceptionHandler

--- src/main/java/br/com/fiap/car/sales/infrastructure/exception/GlobalExceptionHandler.java
+++ src/main/java/br/com/fiap/car/sales/infrastructure/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.fiap.car.sales.infrastructure.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.webjars.NotFoundException;
 
 @ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
@@ -16,6 +18,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<String> handleException(Exception e) {
+        log.error("Internal server error: ", e);
         return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
