2e3a7340bc8394fb73eb87e59696663d3e2a5827
wienerdev <matheuswiener9@gmail.com>
2024-07-14 11:12:01 -0300

    Ajustando rota do fiap-car-reg

--- src/main/java/br/com/fiap/car/sales/adapter/out/feign/VehicleRegClient.java
+++ src/main/java/br/com/fiap/car/sales/adapter/out/feign/VehicleRegClient.java
@@ -3,17 +3,17 @@ package br.com.fiap.car.sales.adapter.out.feign;
 import br.com.fiap.car.sales.application.dto.VehicleDto;
 import br.com.fiap.car.sales.application.dto.request.UpdateVehicleStatusRequest;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @FeignClient(value = "fiap-car-reg", url = "${spring.feign.client.fiap-car-reg.url}")
 public interface VehicleRegClient {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping(value = "/vehicle")
     List<VehicleDto> findAllVehicles();
 
-    @RequestMapping(method = RequestMethod.PUT)
+    @PostMapping(value = "/vehicle")
     VehicleDto updateVehicleStatus(UpdateVehicleStatusRequest dto);
 }
